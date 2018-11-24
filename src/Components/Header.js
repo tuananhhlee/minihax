60f0360e7ced478c73d149cb39702c6ed0bf620b
tuananhhlee <tuananhlee2111@gmail.com>
2018-11-24 12:48:06 +0700

    fdfdfdfdfdfdfdfdf
    dfdfdfdfdfdfdfdffdfdfd

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default()=>{
+    return (
+        <header className = "header">
+            <h3 style={{
+                borderBottomWidth: 1,
+                borderStyle: "solid",
+                borderColor: "#cecece"
+            }}><a href="/">Score keeper</a></h3>
+        </header>
+    )
+}
+
+
---
+++ src/Components/Loading.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import "./loading.css"
+
+
+export default (props) =>{
+    return(
+        <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+    )
+};
